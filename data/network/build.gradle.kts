643e527b7729be8af9ea9383d812d0c4ec5769e7
Solutionium <solutionium@gmail.com>
2026-02-17 16:14:49 +0400

    add ktor dependency

--- data/network/build.gradle.kts
+++ data/network/build.gradle.kts
@@ -29,5 +29,18 @@ dependencies {
     implementation(libs.retrofit.kotlinx.serilization.converter)
     implementation("com.squareup.okhttp3:logging-interceptor:4.12.0") // Use the latest version
 
+    val ktorVersion = "2.3.12" // Use the latest stable version
+
+    // Core and Android Engine
+    implementation(libs.ktor.client.core)
+    implementation(libs.ktor.client.android) // Or ktor-client-okhttp
+
+    // Serialization (replaces Retrofit converters)
+    implementation(libs.ktor.client.content.negotiation)
+    implementation(libs.ktor.serialization.kotlinx.json)
+
+    // Logging (replaces HttpLoggingInterceptor)
+    implementation(libs.ktor.client.logging)
+
 
 }
