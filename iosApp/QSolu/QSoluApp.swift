4889e4fbfaabcb1e2c3577148002f9fbd5fc948c
Solutionium <solutionium@gmail.com>
2026-02-25 00:42:22 +0400

    ios app creation

---
+++ iosApp/QSolu/QSoluApp.swift
@@ -0,0 +1,17 @@
+//
+//  QSoluApp.swift
+//  QSolu
+//
+//  Created by Farid Rahimi on 25/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct QSoluApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
